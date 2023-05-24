7ba3439059c6e63a3e6f3b5315bc63300a13ee76
Малик <poseydon100500@gmail.com>
2023-05-25 02:14:09 +0500

    Task2

---
+++ Example002_Task2/Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1)
+{
+    System.Console.WriteLine("Понедельник");
+}
+else if (num == 2)
+{
+    System.Console.WriteLine("Вторник");
+}
+else if (num == 3)
+{
+    System.Console.WriteLine("Среда");
+}
+else if (num == 4)
+{
+    System.Console.WriteLine("Четверг");
+}
+else if (num == 5)
+{
+    System.Console.WriteLine("Пятница");
+}
+else if (num == 6)
+{
+    System.Console.WriteLine("Суббота");
+}
+else if (num == 7)
+{
+    System.Console.WriteLine("Воскресенье");
+}   
+else
+{
+    System.Console.WriteLine("Такого дня недели нет");
+}
---
+++ Example002_Task2/Task7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+//   456 -> 6
+//   782 -> 2
+//   918 -> 8
+
+System.Console.WriteLine("Введите первое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number >= 100)
+{
+
+while(number / 1000 > 0)
+{
+    number /= 10;
+}
+int digit = number %10;
+System.Console.WriteLine(digit);
+}
+else System.Console.WriteLine("Третьей цифры нет");
