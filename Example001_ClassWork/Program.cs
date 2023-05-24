b45412cee68262d5404f82da3fe9253cbc7d07d1
Малик <poseydon100500@gmail.com>
2023-05-25 00:09:01 +0500

    ClassWork01

---
+++ Example001_ClassWork/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное
+// на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+System.Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num * num;
+System.Console.WriteLine("Квадрат введеного числа равен" + result);
+
+
+if (num > 0)
+{
+    System.Console.WriteLine("Вы ввели положительное число");
+}
+else if (num == 0)
+    System.Console.WriteLine("Вы ввели ноль");
+else
+    System.Console.WriteLine("Вы ввели отрицательное число");
