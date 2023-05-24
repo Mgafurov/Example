c862d6110f3b92a1b997be47a5ca49577f8daf2e
Малик <poseydon100500@gmail.com>
2023-05-24 18:06:42 +0500

    add randon

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 55;
+﻿int numberA = new Random().Next(1, 10); //1 2 3 4 ... 9
+System.Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+System.Console.WriteLine(numberB);
 int result = numberA + numberB;
 System.Console.WriteLine(result);
